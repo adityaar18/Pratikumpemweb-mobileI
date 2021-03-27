21a86f9cee64b986f8bfc1c73d7b1c1b98315bcd
adityaar18 <adityaanandar15@gmail.com>
2021-03-27 21:58:52 +0700

    Modul 1 Pemweb

--- Modul_1/index.php
+++ Modul_1/index.php
@@ -1,71 +1,37 @@
 <?php
-function hitung_vocal($kata){
-  $arr = str_split($kata);
-  $vocal = ['a', 'i', 'u', 'e', 'o'];
-  $found = array_intersect($vocal, $arr);
-  $count = array_count_values($arr);
-  $result = [];
-  foreach ($found as $item) {
-      $result[$item] = $count[$item];
-    }
-    return $result;
+$family=["marisa kristina","daniel fernandez roha","xavier hernandez","messi","yan valery","paulo escobar"];
+
+foreach ($family as $nama) {
+  echo "<br>Jumlah kata dari nama "."$nama"." : ".str_word_count($nama)."<br>";
 }
 
-function hitung_kos($kata1){
-    $arr1 = str_split($kata1);
-    $kos = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z'];
-    $found1 = array_intersect($kos, $arr1);
-    $count1 = array_count_values($arr1);
-    $result1 = [];
-    foreach ($found1 as $item1) {
-        $result1[$item1] = $count1[$item1];
-    }
-    return $result1;
+foreach ($family as $nama) {
+  $split = str_replace(" ","", $nama);
+  echo "<br>Jumlah huruf dari nama "."$nama"." : ".strlen($nama)."<br>";
+}
+
+foreach ($family as $nama) {
+  echo "<br>Jumlah kebalikan dari nama "."$nama"." : ".strrev($nama)."<br>";
+}
+
+function vocal_konstanta($word){
+  $word_len = strlen($word) - 1;
+  $vocal_words = array("a","e","i","o","u");
+  
+  $vocal = 0;
+  $konstantan = 0;
+
+  for($i = 0; $i <= $word_len; $i++){
+      if(in_array($word[$i], $vocal_words)){
+          $vocal++;
+      }else{
+          $konstantan++;
+      }
+  }   
+  echo "<br>jumlah huruf vocal $word : ".$vocal."<br>";
+  echo "<br>jumlah huruf konsonant $word : ".$konstantan."<br>";
+}
+foreach($family as $data){
+	vocal_konstanta($data);
 }
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-    <link rel="stylesheet" href="form.css">
-  <title>Tugas Pemograman Web</title>
-</head>
-<body>
-  <h1>Hitung Huruf Vocal dan Kosonan</h1>
- 
-  <form method="POST" action="<?php $PHP_SELF ?>">
-    <p>
-      Kata: <br>
-      <input type="text" name="kata">
-    </p>
-    <p>
-      <button type="submit">Hitung</button>
-    </p>
-  </form>
- 
-  <?php if ($_POST && isset($_POST['kata'])): ?>
-    <hr>
- 
-    <table border="1">
-      <?php foreach (hitung_vocal($_POST['kata']) as $huruf => $jumlah): ?>
-        <tr>
-          <td style="width:100px">Huruf  : <?php echo $huruf ?></td>
-          <td style="width:100px">Jumlah : <?php echo $jumlah ?></td>
-        </tr>
-      <?php endforeach ?>
-    </table>
-  <?php endif ?>
-  <?php if ($_POST && isset($_POST['kata'])): ?>
-    <hr>
- 
-    <table border="1">
-      <?php foreach (hitung_kos($_POST['kata']) as $huruf1 => $jumlah1): ?>
-        <tr>
-          <td style="width:100px">Huruf  : <?php echo $huruf1 ?></td>
-          <td style="width:100px">Jumlah : <?php echo $jumlah1 ?></td>
-        </tr>
-      <?php endforeach ?>
-    </table>
-  <?php endif ?>
-</body>
-</html>
