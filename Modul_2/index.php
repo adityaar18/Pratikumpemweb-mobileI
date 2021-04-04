89e46c0ad8cccbca70eefa8213b140ba458f10f8
adityaar18 <adityaanandar15@gmail.com>
2021-04-04 23:52:04 +0700

    Modul_2

--- Modul_2/index.php
+++ Modul_2/index.php
@@ -1,12 +1,3 @@
-<!DOCTYPE HTML>  
-<html>
-<head>
-<style>
-.error {color: #FF0000;}
-</style>
-</head>
-<body>  
-
 <?php
 $username_h = $password_h = $usernameErr = $passwordErr = $berhasil = "";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -15,36 +6,30 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $user = strlen($username);
   $pass = strlen($password);
   $x = false;
-  if (empty($_POST["username"])) {
-    $usernameErr = "Username perlu di isi";
-  }
   if($user>7){
-    $usernameErr = "Username lebih dari 7";
+    $usernameErr = "Username lebih dari 7 karakter";
     $x = true;
 }
-if (empty($_POST["password"])) {
-  $passwordErr = "password perlu di isi";
-}
 if (!preg_match("/[A-Z]/", $password) ) {
-  $passwordErr = "password kapital";
+  $passwordErr = "password tidak memiliki huruf kapital";
     $x = true;
 }
 if (!preg_match("/[a-z]/", $password)) {
-  $passwordErr = "password kecil";
+  $passwordErr = "password  tidak memiliki huruf kecil";
     $x = true;
 }
 
 if (!preg_match("/[^a-zA-Z\d]/", $password)) {
-  $passwordErr = "password special character";
+  $passwordErr = "password  tidak memiliki special character";
     $x = true;
 }
 
 if (!preg_match("/[0-9]/", $password)) {
-  $passwordErr = "password digit";
+  $passwordErr = "password  tidak memiliki digit/angka";
     $x = true;
 }
 if($pass>10){
-  $passwordErr = "password lebih dari 10";
+  $passwordErr = "password lebih dari 10 karakter";
     $x = true;
 }
 if( $x == false ){
@@ -55,6 +40,14 @@ if( $x == false ){
 }
 ?>
 
+<!DOCTYPE HTML>  
+<html>
+<head>
+<style>
+.error {color: #FF0000;}
+</style>
+</head>
+<body>  
 <h2>FORM VALIDASI</h2>
 <p><span class="error">* WAJIB DI ISI.</span></p>
 <form method="post" action="<?php echo ($_SERVER["PHP_SELF"]);?>">  
@@ -66,13 +59,13 @@ if( $x == false ){
   <br><br>
   <input type="submit" name="submit" value="Submit">  
 </form>
-
 <?php
 echo $berhasil;
+if( $x == false ){
 echo "Username : ".$username_h;
 echo "<br>";
 echo "Password : ".$password_h;
+}
 ?>
-
 </body>
 </html>
